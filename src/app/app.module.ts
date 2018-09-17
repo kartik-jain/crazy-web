a0d049a639c5de8dcfd1ef61fa71ca9816e7d899
Kunwar Ashutosh Singh <kunwarashutoshsingh97@gmail.com>
2018-09-17 22:36:27 +0530

    added app service & header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { TruncatePipe } from './pipes/truncate.pipe';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { SignupComponent } from './components/signup/signup.component';
     AppComponent,
     TruncatePipe,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    DashboardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
