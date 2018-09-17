e4b577d86b710967e4ea7df552b48a6fac1f54ff
Kunwar Ashutosh Singh <kunwarashutoshsingh97@gmail.com>
2018-09-17 22:35:14 +0530

    added auth screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TruncatePipe } from './pipes/truncate.pipe';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TruncatePipe,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
