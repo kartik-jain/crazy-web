ccc672287964c81a5902355a0118cd52032a6c1a
Kunwar Ashutosh Singh <kunwarashutoshsingh97@gmail.com>
2018-09-17 22:40:06 +0530

    added more components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { SignupComponent } from './components/signup/signup.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BootstrapComponent } from './components/bootstrap/bootstrap.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 
 @NgModule({
@@ -19,7 +23,11 @@ import { BootstrapComponent } from './components/bootstrap/bootstrap.component';
     SignupComponent,
     DashboardComponent,
     HeaderComponent,
-    BootstrapComponent
+    BootstrapComponent,
+    ProfileComponent,
+    FooterComponent,
+    AboutComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
